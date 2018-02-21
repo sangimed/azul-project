65d76366864b7c4ebcf4153c4a1975554f65138b
Mohamed IBRIHEN <sangimed@gmail.com>
2018-02-21 17:09:08 +0100

    drafting

--- content.js
+++ content.js
@@ -1,13 +1,18 @@
 // content.js
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      if( request.message === "clicked_browser_action" ) {
-        var firstHref = $("a[href^='http']").eq(0).attr("href");
-  
-        console.log(firstHref);
-  
-        // This line is new!
-        chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-      }
+
+// Globals
+var previousChar = "";
+
+// Functions
+document.addEventListener('keypress', (event) => {
+    const currentChar = event.key;
+    
+    if ("" !== previousChar) {
+        console.log('currentChar : ' + currentChar + '\npreviousChar : ' + previousChar);
+        
+        previousChar = currentChar;
+    } else {
+        previousChar = currentChar;
+        console.log(' [ELSE] currentChar : ' + currentChar + '\npreviousChar : ' + previousChar);
     }
-  );
+  });
