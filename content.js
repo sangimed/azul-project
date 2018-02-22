193126bddcfdeacbf23cc20cf6e2cb07fbd36d19
Mohamed IBRIHEN <sangimed@gmail.com>
2018-02-22 17:24:38 +0100

    draft

--- content.js
+++ content.js
@@ -6,10 +6,13 @@ var previousChar = "";
 // Functions
 document.addEventListener('keypress', (event) => {
     const currentChar = event.key;
-    
+    const currentElement = document.activeElement.tagName;
+
+    console.log("========== " + currentElement);
+
     if ("" !== previousChar) {
         console.log('currentChar : ' + currentChar + '\npreviousChar : ' + previousChar);
-        
+        // LOGIC HERE
         previousChar = currentChar;
     } else {
         previousChar = currentChar;
